db84c7176bdb158ca23b6db8ae10c0a0455f6d6b
sunkh964 <sunkh964@gmail.com>
2024-08-20 14:18:05 +0900

    잠와

---
+++ src/test/java/com/green/FinalTest/FinalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.FinalTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
